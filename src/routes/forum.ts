8b7caf5537a599cf47b513b591491f1b2fe9e65e
Colten Cross <colten@cwcross.com>
2024-04-16 23:34:28 -0400

    Forum route clean up

--- src/routes/forum.ts
+++ src/routes/forum.ts
@@ -1,48 +1,10 @@
 import { axios } from "~/lib/axios";
 import { enforceArray } from "~/lib/helpers";
-import { thing, family } from "~/routes/types";
 
 type Params = {
   id: string;
   page?: string;
 };
-
-/*
-{
-  "_declaration": { "_attributes": { "version": "1.0", "encoding": "utf-8" } },
-  "forum": {
-    "_attributes": {
-      "id": "1565736",
-      "title": "Reviews",
-      "numthreads": "177",
-      "numposts": "3185",
-      "lastpostdate": "Thu, 01 Jan 1970 00:00:00 +0000",
-      "noposting": "0",
-      "termsofuse": "https://boardgamegeek.com/xmlapi/termsofuse"
-    },
-    "threads": {
-      "thread": [
-        {
-          "_attributes": {
-            "id": "3265741",
-            "subject": "You can't say Scythe without sigh",
-            "author": "LyallUClarion",
-            "numarticles": "20",
-            "postdate": "Thu, 14 Mar 2024 20:23:23 +0000",
-            "lastpostdate": "Fri, 22 Mar 2024 16:38:46 +0000"
-          }
-        },
-        {
-          "_attributes": {
-            "id": "3254495",
-            "subject": "Detailed Scythe Review - Spoiler Free",
-            "author": "IndianBoardGamer",
-            "numarticles": "1",
-            "postdate": "Sun, 25 Feb 2024 19:45:52 +0000",
-            "lastpostdate": "Sun, 25 Feb 2024 19:45:53 +0000"
-          }
-        },
-*/
 
 type ApiResponse = {
   forum: {
